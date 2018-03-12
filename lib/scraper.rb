a5420a87350a4638dd9d725a5995723b4be81551
Brent  Ransom <sadumedusa@gmail.com>
2018-03-12 03:50:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,9 +19,27 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    profile = Nokogiri::HTML(open(profile_url))
+    profile_page = Nokogiri::HTML(open(profile_url))
 
-    
+    profiles = {}
+
+    link = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
+    social_media_link.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+
+    profiles[:quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote").text
+
+    profiles[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder").text 
+    if profile_page.css("div.bio-content.content-holder div.description-holder").text
   end
 
 end
