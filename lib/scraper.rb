ae25eb4c31b5bb24621e24dd2e20673534dead11
Brent  Ransom <sadumedusa@gmail.com>
2018-03-12 03:31:29 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = Nokogiri::HTML(open(index_url))
 
+    student = []
+
+    html.css(".student-card a").each do |student|
+      profile = "#{student.attr('href')}"
   end
 
   def self.scrape_profile_page(profile_url)
