7ce02de61ef5d20b976c6b0904335b49df7ff57f
Brent  Ransom <sadumedusa@gmail.com>
2018-03-12 03:33:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,14 +6,15 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = Nokogiri::HTML(open(index_url))
 
-    student = []
+    students = []
 
     html.css(".student-card a").each do |student|
       profile = "#{student.attr('href')}"
       location = student.css(".student-location").text
       name = student.css(".student-name").text
 
-      
+      students << {name: name, location: location, profile_url: profile}
+
   end
 
   def self.scrape_profile_page(profile_url)
